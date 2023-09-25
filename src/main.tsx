b6db6cc863a92e24f1c690c2aae1791b46d13cb2
123Fang <38906235+123Fang@users.noreply.github.com>
2023-09-25 16:26:58 +0800

    初始化组件文档

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import RouterView from './router';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <RouterView />
+);
---
+++ src/router/pages/page.tsx
@@ -0,0 +1,11 @@
+// 路由配置
+import HomePage from '../../pages/home';
+
+
+const BaseRouter: any = [
+  {
+    path: '/*',
+    element: <HomePage />,
+  }
+];
+export default BaseRouter;
